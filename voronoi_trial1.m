cea8d5665f0eaa3a1a3cb754b75e79ee78fd8436
Rianna Jitosho <rjitosho@stanford.edu>
2021-02-28 13:09:48 -0800

    update voronoi

--- voronoi_trial1.m
+++ voronoi_trial1.m
@@ -6,6 +6,7 @@ vmax = 1; % max velocity for pursuers and evaders
 
 P = [0.5 0; 0 0.5; -0.5 -0.5; -0.2 -0.1; -0.1 0.1; 0.1 -0.1; 0.1 0.1];
 calculate_evader_velocity(P(:, 1), P(:, 2), 4:5)
+calculate_pursuer_velocity(P(:, 1), P(:, 2), 4:5, 6:7)
 
 function u_e = calculate_evader_velocity(pos_x, pos_y, inds_e)
 % pos_x: x-coordinates of all positions in a column vector
@@ -13,16 +14,17 @@ function u_e = calculate_evader_velocity(pos_x, pos_y, inds_e)
 % inds_e: range of indices in pos_x and pos_y that are evaders (eg. 1:3)
 
 [vx, vy] = voronoi(pos_x, pos_y);
+square = polyshape(2*[-.5 -.5 0.5 0.5],2*[-.5 .5 .5 -.5]);
 [vertices, indices] = voronoin([pos_x pos_y]);
-u_e = [];
 
+u_e = [];
 for i=inds_e
     cell_shape = polyshape(vertices(indices{i}, 1), vertices(indices{i}, 2));
     Cvi = centroid(cell_shape);
     xe = [pos_x(i), pos_y(i)];
-    u_e = [u_e; (Cvi - xe)/norm(Cvi - xe)];    
+    u_e = [u_e (Cvi - xe)/norm(Cvi - xe)];    
 end
-
+u_e = u_e';
 end
 
 function u_p = calculate_pursuer_velocity(pos_x, pos_y, inds_p, inds_e)
@@ -31,8 +33,9 @@ function u_p = calculate_pursuer_velocity(pos_x, pos_y, inds_p, inds_e)
 % inds_p: range of indices in pos_x and pos_y that are pursuers (eg. 1:3)
 
 [vertices, indices] = voronoin([pos_x pos_y]);
-
+u_p = [];
 for i=inds_p
+    p_pos = [pos_x(i), pos_y(i)];
     neighbors_e = [];
     for j=inds_e
         s = size(intersect(indices{i}, indices{j}));
@@ -43,9 +46,39 @@ for i=inds_p
     
     % if the pursuer has neighboring evaders
     if (length(neighbors_e) > 1)
+        % find nearest one
+        nearest_dist = Inf;
+        nearest_edge = [0,0];
+        for j = neighbors_e            
+            e_pos = [pos_x(j), pos_y(j)];
+            if norm(p_pos-e_pos) < nearest_dist
+                nearest_dist = norm(p_pos-e_pos);
+                nearest_edge = intersect(indices{i}, indices{j});
+            end
+        end
+        
+        % u = (Cbj -xpj)/||Cbj -xpj||
+        vertex_1 = vertices(nearest_edge(1));
+        vertex_2 = vertices(nearest_edge(2));
+        Cbj = .5*(vertex_1+vertex_2);
+        u_p = [u_p (Cbj - p_pos)/norm(Cbj - p_pos)];
+        
     % if the pursuer has no neighboring evaders
     else
+        % find nearest
+        nearest_dist = Inf;
+        nearest_pos = [0,0];
+        for j = neighbors_e            
+            e_pos = [pos_x(j), pos_y(j)];
+            if norm(p_pos-e_pos) < nearest_dist
+                nearest_dist = norm(p_pos-e_pos);
+                nearest_pos = e_pos;
+            end
+        end
+        
+        u_p = [u_p (nearest_pos - p_pos)/norm(nearest_pos - p_pos)];
     end
 end
+u_p = u_p';
 
 end
